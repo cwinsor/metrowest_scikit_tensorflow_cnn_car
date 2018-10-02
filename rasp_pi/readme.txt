db3237090f69c34538de8364d4a5e53909a36e87
Chris Winsor <cwinsor@gmail.com>
2018-10-01 22:28:44 -0400

    updates to the run python car.py

---
+++ rasp_pi/readme.txt
@@ -0,0 +1,39 @@
+
+Instructions to install Keras on Raspberry Pi...
+
+https://medium.com/@abhizcc/installing-latest-tensor-flow-and-keras-on-raspberry-pi-aac7dbf95f2
+
+Install Keras:
+sudo apt-get install python3-numpy
+sudo apt-get install libblas-dev
+sudo apt-get install liblapack-dev
+sudo apt-get install python3-dev 
+sudo apt-get install libatlas-base-dev
+sudo apt-get install gfortran
+sudo apt-get install python3-setuptools
+sudo apt-get install python3-scipy
+sudo apt-get update
+sudo apt-get install python3-h5py
+sudo pip3 install keras 
+
+
+Test your Keras installation by
+python -c 'import keras; print(keras.__version__)'# python 2
+python3 -c 'import keras; print(keras.__version__)'  # for Python 3
+
+
+For Tensorflow there is an additional step necessary
+it involves adding a /etc/pip.conf file that points to www.piwheels.org/simple
+https://www.raspberrypi.org/magpi/tensorflow-ai-raspberry-pi/
+https://www.raspberrypi.org/forums/viewtopic.php?t=219853
+
+sudo nano /etc/pip.conf
+
+and add
+extra-index-url=https://www.piwheels.org/simple
+
+and then run the pip3 install command.
+
+Test your Tensorflow installation by
+python -c 'import tensorflow as tf; print(tf.__version__)'  # for Python 2
+python3 -c 'import tensorflow as tf; print(tf.__version__)'  # for Python 3
--- rasp_pi/self_driving_car_drive.py
+++ rasp_pi/self_driving_car_drive.py
@@ -4,6 +4,14 @@ from picamera import PiCamera
 from time import sleep
 from time import time
 import numpy as np
+import keras
+import os
+from keras import models
+import time
+import picamera
+import picamera.array
+
+print("keras version " + keras.__version__)
 
 # Self-driving car for Metrowest Machine Learning Group.
 #
@@ -23,52 +31,59 @@ pin_right = 13
 pin_l = OutputDevice(pin_left,True,True)
 pin_r = OutputDevice(pin_right,True,True)
 
-camera = PiCamera()
-camera.resolution = (64, 64)
-#  camera.framerate = 24
-camera.rotation = 180
-#  camera.raw_format='rgb'
-camera.start_preview()
-sleep(5)
-camera.stop_preview()
+with picamera.PiCamera() as camera:
+    camera.resolution = (64, 64)
+    camera.rotation = 180
+    camera.start_preview()
+    sleep(5)
+    camera.stop_preview()
+    print("camera preview is complete")
+
+    save_dir = os.path.join(os.getcwd(), '../model/saved_models/')
+    model_name = 'metrowest_keras_trained_model.h5'
+    model_path = os.path.join(save_dir, model_name)
+    model = keras.models.load_model(model_path)
+    print('load of CNN model is complete')
 
 
 
-while True:
-    
-    # capture image
-    camera.capture(image, resize=(64, 64), use_video_port=True)
+# from https://picamera.readthedocs.io/en/release-1.10/recipes2.html#unencoded-image-capture-rgb-format
+    with picamera.array.PiRGBArray(camera) as stream:
+        camera.capture(stream, 'rgb', resize=(64,64), use_video_port=True)
+        # Show size of RGB data
+        print(stream.array.shape)
 
-    # the CNN expects (was trained using) an array of float32 normalized images
-    image = image.astype('float32')
-    image /= 255
-    image = image[np.newaxis, :]
+        image = stream
 
-    # predict direction of track (the desired steering direction)
-    steering_out = model.predict(image)
-    print(steering_out)
+        # the CNN expects (was trained using) an array of float32 normalized images
+        image = image.astype('float32')
+        image /= 255
+        image = image[np.newaxis, :]
 
-    # apply to GPIOs
-    s_max = np.where(steering_out==steering_out.max())
-    s_index = s_max[0][0]
-    if s_index == 0:
-        print("left")
-        pin_l.off()
-        pin_r.on()
-    elif s_index == 1:
-        print("center")
-        pin_l.on()
-        pin_r.on()
-    elif s_index == 2:
-        print("right")
-        pin_l.on()
-        pin_r.off()
-    else:
-        assert False, "something wrong in prediction %r" % s_index
+        # predict direction of track (the desired steering direction)
+        steering_out = model.predict(image)[0]
+        s_max = np.where(steering_out==steering_out.max())
+        s_index = s_max[0][0]
 
-    # hold for 0.5 sec - then release to center
-    sleep(0.5)
-    pin_l.on()
-    pin_r.on()
+        # apply to GPIOs
+        if s_index == 1:
+            print("left")
+            pin_l.off()
+            pin_r.on()
+        elif s_index == 2:
+            print("right")
+            pin_l.on()
+            pin_r.off()
+        elif s_index == 3:
+            print("center")
+            pin_l.on()
+            pin_r.on()
+        else:
+            assert False, "something wrong in prediction %r" % s_index
+
+        # hold for a bit
+        sleep(0.2)
+        #pin_l.on()
+        #pin_r.on()
     
     
