b08b444e9324fa8cab05b32284b238f0aacbc5b9
Chris Winsor <cwinsor@gmail.com>
2019-02-09 20:26:25 +0000

    move raspberry pi to auto-run using /etc/systemd/system/metrowest_car_blah_blah this automatically starts the carw

--- run_the_car.sh
+++ run_the_car.sh
@@ -1,12 +1,31 @@
 #!/bin/bash -e
 
+# This script can be used to automatically run the car
+# when the Pi boots up. It is intended to be called using
+# a systemd service unit the systemd service files are 
+# in /etc/systemd/
+#
+# For details on how to set up a systemd service unit refer to 
+# https://linuxconfig.org/how-to-automatically-execute-shell-script-at-startup-boot-on-systemd-linux
+#
+# Commands to monitor status of systemctl/systemd are:
+# sudo systemctl daemon-reload
+# sudo systemctl start metrowest-car-start-it-running.service
+# sudo systemctl status metrowest-car-start-it-running.service
+# sudo systemctl stop metrowest-car-start-it-running.service
+
+
 export PROJECT_HOME="/home/pi/metrowest_scikit_tensorflow_cnn_car"
 
-alias setup_00="source ${PROJECT_HOME}/project/rasp_pi/setup_everytime_pi_00_activate_env.sh"
-alias setup_01="source ${PROJECT_HOME}/project/rasp_pi/setup_everytime_pi_01_add_lib_to_path.sh"
-alias run_it="python3 ${PROJECT_HOME}/project/rasp_pi/self_driving_car_drive.py &"
+echo "here1 - before activate env"
+echo $(which python)
+source ${PROJECT_HOME}/project/rasp_pi/setup_everytime_pi_00_activate_env.sh
+echo "here2 - after activate env, before add lib"
+echo $(which python)
+source ${PROJECT_HOME}/project/rasp_pi/setup_everytime_pi_01_add_lib_to_path.sh
+echo "here3"
+python ${PROJECT_HOME}/project/rasp_pi/self_driving_car_drive.py > /home/pi/metrowest_scikit_tensorflow_cnn_car/run_output.log 2>&1
+#python ${PROJECT_HOME}/project/rasp_pi/self_driving_car_drive.py
+echo "here4"
 
-setup_00
-setup_01
-echo "python is " $(which python)
-run_it
+
